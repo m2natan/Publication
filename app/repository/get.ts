45b6a45a6553fdf34074a225f43cb14039dc07f8
nmesay <nmesay@kifiya.com>
2025-09-22 13:34:32 +0300

    added updatedAt for easy for publication

--- app/repository/get.ts
+++ app/repository/get.ts
@@ -1,22 +1,31 @@
 import { prisma } from "@/lib/prisma";
 
 export async function getPublication() {
+  const now = new Date();
+  const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+  const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+
   return await prisma.publication.findMany({
+    where: {
+      createdAt: {
+        gte: startOfMonth, // greater than or equal to first day of month
+        lt: endOfMonth, // less than first day of next month
+      },
+    },
+    orderBy: { updatedAt: "desc" },
   });
 }
 
-export async function getPublicationBySearch(search?: string){
-  if(search){
+export async function getPublicationBySearch(search?: string) {
+  if (search) {
     return await prisma.publication.findMany({
-      where:{
-        OR:[
-          {publication_id: {contains: search, mode: 'insensitive'}}
-        ],
+      where: {
+        OR: [{ publication_id: { contains: search, mode: "insensitive" } }],
       },
-      orderBy: {createdAt: 'desc'},
-    })
-  }else{
-    return
+      orderBy: { updatedAt: "desc" },
+    });
+  } else {
+    return;
   }
 }
 
